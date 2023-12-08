cd663f45ea05537c28c91f3014a9dd6287665712
Gabriel Donizete dos Santos Lima <gabrieldonizete0@gmail.com>
2023-12-08 16:08:59 -0300

    Classe Exception, para mensagem no retorno das exeções.
    Signed-off-by: Gabriel Donizete dos Santos Lima <gabrieldonizete0@gmail.com>

---
+++ bitcoin/src/main/java/br/com/projeto/bitcoin/exception/BitcoinException.java
@@ -0,0 +1,8 @@
+package br.com.projeto.bitcoin.exception;
+
+public class BitcoinException extends RuntimeException{
+	
+	public BitcoinException(String message) {
+        super(message);
+    }
+}
