35f2cebbd0866bc45f0cc0259bdfe68f6de772aa
Gabriel Donizete dos Santos Lima <gabrieldonizete0@gmail.com>
2023-12-16 01:37:47 -0300

    Classe SaldoDTO
    Signed-off-by: Gabriel Donizete dos Santos Lima <gabrieldonizete0@gmail.com>

---
+++ bitcoin/src/main/java/br/com/projeto/bitcoin/dto/SaldoDTO.java
@@ -0,0 +1,22 @@
+package br.com.projeto.bitcoin.dto;
+
+import java.math.BigInteger;
+
+public class SaldoDTO {
+	
+	private BigInteger saldoConfirmado;
+    private BigInteger saldoNaoConfirmado;
+    
+	public BigInteger getSaldoConfirmado() {
+		return saldoConfirmado;
+	}
+	public void setSaldoConfirmado(BigInteger saldoConfirmado) {
+		this.saldoConfirmado = saldoConfirmado;
+	}
+	public BigInteger getSaldoNaoConfirmado() {
+		return saldoNaoConfirmado;
+	}
+	public void setSaldoNaoConfirmado(BigInteger saldoNaoConfirmado) {
+		this.saldoNaoConfirmado = saldoNaoConfirmado;
+	}  
+}
